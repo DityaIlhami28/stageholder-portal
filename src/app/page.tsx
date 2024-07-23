a3400a7da9eefefc837422e5a8c4272bc5ac6438
“maulanaFandi” <maulana27fandi@gmail.com>
2024-07-24 04:51:19 +0700

    Fix: landingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import LandingPageContainer from '@/components/landingPageContainer';
 import CardHomeContent from '@/components/cardHomeContent';
-import { Box } from '@mui/material';
+import { Box, Divider } from '@mui/material';
+import LandingPagePrice from '@/components/landingPagePrice';
 
 const Home = () => {
   return (
     <>
+      <Divider />
       <LandingPageContainer />
+      <Divider />
+      {/* <CardHomeContent /> */}
+      <Divider />
+      <LandingPagePrice />
+      <Divider />
     </>
   );
 };
---
+++ src/components/landingPagePrice.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+import { Typography } from '@mui/material';
+
+const LandingPagePrice = () => {
+    return(
+        <>
+        <Typography>Ini Buat Price</Typography>
+        </>
+    )
+}
+
+export default LandingPagePrice;
